9e29eb22a2613152dffe59f8ce4dd1f6e313ea7f
Federico Bonelli <federico.bonelli@moxoff.com>
2014-11-15 23:24:15 +0100

    now we call the master directly, so logs will work in docker

--- run-master.sh
+++ run-master.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
-/opt/spark-1.1.0-bin-hadoop2.4/sbin/start-master.sh
-PID="$(cat /tmp/spark-*.pid)"
-ps -p $PID > /dev/null
-while [[ ${?} == 0 ]]      # Repeat until the process has terminated.
-do
-    sleep 10s              # Wait a bit before testing.
-    ps -p $PID >/dev/null  # Check if the process has terminated.
-done
+sbin=/opt/spark-1.1.0-bin-hadoop2.4/sbin
 
+. "$sbin/spark-config.sh"
+
+. "$SPARK_PREFIX/bin/load-spark-env.sh"
+
+if [ "$SPARK_MASTER_PORT" = "" ]; then
+     SPARK_MASTER_PORT=7077
+fi
+
+if [ "$SPARK_MASTER_IP" = "" ]; then
+     SPARK_MASTER_IP=`hostname`
+fi
+
+if [ "$SPARK_MASTER_WEBUI_PORT" = "" ]; then
+     SPARK_MASTER_WEBUI_PORT=8080
+fi
+
+. "$SPARK_PREFIX/bin/load-spark-env.sh"
+
+if [ "$SPARK_IDENT_STRING" = "" ]; then
+   export SPARK_IDENT_STRING="$USER"
+fi
+
+"$SPARK_PREFIX"/bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
